d270f22b35731191b14f5ff77b2c752a65413b8d
Gracia Mobongol <mayaya-gracia.mobongol.1@ens.etsmtl.ca>
2025-09-09 15:54:11 -0400

    étape 3 - test

--- test/routes/jeuRouter-redemarrerJeu-lab0.test.ts
+++ test/routes/jeuRouter-redemarrerJeu-lab0.test.ts
@@ -1,9 +1,29 @@
 // Vous devez insérer les nouveaux tests ici
 import { assert } from 'console';
 import 'jest-extended';
+import supertest from 'supertest';
+import app from '../../src/app';
+
+const request = supertest(app);
+
+const testNom1 = 'Jean-Marc';
+const testNom2 = 'Pierre';
+
+beforeAll(async () => {
+  await request.post('/api/v1/jeu/demarrerJeu').send({ nom: testNom1 });
+  await request.post('/api/v1/jeu/demarrerJeu').send({ nom: testNom2 });
+});
 
 describe('redemarrerJeu.test.ts', () => {
   it("devrait implémenter test", async () => {
     throw new Error("Ce test n'a pas été défini")
+  });
+});
+
+describe('GET /api/v1/jeu/redemarrerJeu', () => {
+  it("devrait valider le succès de l'opération", async () => {
+    const response = await request.get('/api/v1/jeu/redemarrerJeu');
+    expect(response.status).toBe(200);
+    expect(response.type).toBe("application/json")
   });
 });
